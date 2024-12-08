fc92008a2f9aa7d47bf4c36c6fc5c802a28c5faa
Nupurbhoir <nupurbhoir21@gmail.com>
2024-12-08 14:17:58 +0530

    Add files via upload

---
+++ ex2classstring.cpp
@@ -0,0 +1,16 @@
+//2. create string using string class (user input) and 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1;
+
+    cout << "Enter string: ";
+    getline(cin, str1);
+
+    cout << "Length of the string: " << str1.length() << endl;
+    cout << "Copied string: " << str1 << endl;
+
+    return 0;
+} 
